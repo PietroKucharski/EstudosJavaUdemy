71d9c5a560beb517f8dab8f3c8863afb9a900473
pietr <pietrokucharski@hotmail.com>
2025-04-11 19:24:48 -0300

    Resolvendo exercício aprovados

--- src/application/Programa13.java
+++ src/application/Programa13.java
@@ -289,7 +289,7 @@ public class Programa13 {
             da pessoa mais velha
         */
 
-        System.out.print("Quantas pessoas você vai digitar? ");
+        /*System.out.print("Quantas pessoas você vai digitar? ");
         int n = sc.nextInt();
 
         String[] nome = new String[n];
@@ -317,6 +317,37 @@ public class Programa13 {
 
         }
 
-        System.out.printf("PESSOA MAIS VELHA: %s", nome[indicePessoaMaisVelha]);
+        System.out.printf("PESSOA MAIS VELHA: %s", nome[indicePessoaMaisVelha]);*/
+
+        /*
+        Exercício 10 - aprovados
+            Fazer um programa para ler um conjunto de N nomes de alunos, bem como as notas que eles tiraram
+            no 1º e 2º semestres. Cada uma dessas informações deve ser armazenada em um vetor. Depois, imprimir
+            os nomes dos alunos aprovados, considerando aprovados aqueles cuja média das notas seja maior ou
+            igual a 6.0 (seis).
+        */
+
+        System.out.print("Quantos alunos serão digitados? ");
+        int n = sc.nextInt();
+
+        String[] nomes = new String[n];
+        double[] primeiraNota = new double[n];
+        double[] segundaNota = new double[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.printf("Digite nome, primeira e segunda nota do %do aluno:\n", i + 1);
+            sc.nextLine();
+            nomes[i] = sc.nextLine();
+            primeiraNota[i] = sc.nextDouble();
+            segundaNota[i] = sc.nextDouble();
+        }
+
+        System.out.println("Alunos aprovados: ");
+
+        for (int i = 0; i < n; i++) {
+            if((primeiraNota[i] + segundaNota[i]) / 2 >= 6.0) {
+                System.out.printf("%s \n", nomes[i]);
+            }
+        }
     }
 }
