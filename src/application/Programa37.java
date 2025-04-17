c7112d0f9bb0cb7fb64e4501ef2c7dcfddf6697a
pietr <pietrokucharski@hotmail.com>
2025-04-17 17:13:07 -0300

    Aprendendo sobre o bloco finally

---
+++ src/application/Programa37.java
@@ -0,0 +1,26 @@
+package application;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Programa37 {
+    public static void main(String[] args) {
+        File file = new File("C:\\Users\\pietr\\Documents\\JavaUdemy\\Arquivos_teste\\in.txt");
+        Scanner sc = null;
+
+        try {
+            sc = new Scanner(file);
+            while (sc.hasNextLine()) {
+                System.out.println(sc.nextLine());
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Error opening file: " + e.getMessage());
+        } finally {
+            if(sc != null) {
+                sc.close();
+            }
+            System.out.println("Finally block executed");
+        }
+    }
+}
