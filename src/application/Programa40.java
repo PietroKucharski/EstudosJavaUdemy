576fe49b14c7c77c977eaf0479cb8d92975e804e
pietr <pietrokucharski@hotmail.com>
2025-04-22 12:17:35 -0300

    Ajustando nomes de arquivos

--- src/application/Programa40.java
+++ src/application/Programa40.java
@@ -1,46 +1,25 @@
 package application;
 
-import entities.Account03;
-import exceptions.BusinessException;
-
-import java.util.Locale;
+import java.io.File;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Programa40 {
     public static void main(String[] args) {
-        /* Exercicio de fixação
-        Fazer um programa para ler os dados de uma conta bancária e depois realizar um saque nesta conta bancária,
-        mostrando o novo saldo. Um saque não pode ocorrer ou se não houver saldo na conta, ou se o valor do saque for
-        superior ao limite de saque da conta
-        Class Account -> number:Integer; holder:String; balance:Double; withdrawLimit:Double;
-                         depoist(Double amount): void; withdraw(amount:Double): void
-        */
-        Locale.setDefault(Locale.US);
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = null;
+        File file = new File("C:\\Users\\pietr\\Documents\\JavaUdemy\\Arquivos_teste\\in.txt");
 
-        System.out.println("Enter account data");
-
-        System.out.print("Number: ");
-        int accountNumber = sc.nextInt();
-
-        System.out.print("Holder: ");
-        String accountHolder = sc.next();
-
-        System.out.print("Initial Balance: ");
-        double initialAccountBalance = sc.nextDouble();
-
-        System.out.print("Withdraw limit: ");
-        double withdrawLimitAccount = sc.nextDouble();
-
-        Account03 account = new Account03(accountNumber, accountHolder, initialAccountBalance, withdrawLimitAccount);
-
-        System.out.print("Enter amount for withdraw: ");
-        double amountWithdraw = sc.nextDouble();
         try {
-            account.withDraw(amountWithdraw);
-            System.out.printf("New balance: %.2f%n", account.getBalance());
-        } catch (BusinessException e) {
-            System.out.println(e.getMessage());
+            sc = new Scanner(file);
+            while (sc.hasNextLine()) {
+                System.out.println(sc.nextLine());
+            }
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        } finally {
+            if (sc != null) {
+                sc.close();
+            }
         }
     }
 }
